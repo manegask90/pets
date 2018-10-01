a3b9c5c7c246a9a67dea69ca4bb04f61347e289a
Vitalik M <vitalikmihaylishin@gmail.com>
2018-10-01 17:10:17 +0300

    commit 01.10 update

--- wp-content/themes/divi-child/news.php
+++ wp-content/themes/divi-child/news.php
@@ -10,6 +10,54 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 ?>
         <?php if ( ! $is_page_builder_used ) : ?>
 
+
+
+    <?php
+
+    $posts = get_posts(array(
+        'numberposts' => 1,
+        'tag' => 'main',
+        'post_type' => 'post',
+        'category' => 6,
+        'suppress_filters' => true
+    ));
+
+    foreach ($posts as $post) { ?>
+        <?php //setup_postdata($post); ?>
+
+
+        <section class="top_news_wrap">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-8 block_1">
+                        <a href="<?php esc_url( the_permalink() ); ?>">
+                            <img src="<?php the_field('main_img'); ?>" alt=""/>
+                        </a>
+                    </div>
+                    <div class="col-md-4 block_2"></div>
+                </div>
+            </div>
+            <div class="container container-2">
+                <div class="row align-items-center">
+                    <div class="col-md-3 block_1_1">
+                        <a href="<?php esc_url( the_permalink() ); ?>">
+                            <h2 class="entry-title"><?php the_title(); ?></h2>
+                            <div class="entry-content">
+                                <?php the_excerpt(); ?>
+                            </div>
+                        </a>
+                    <?php } wp_reset_postdata(); // сброс ?>
+                    </div>
+                    <div class="col-md-3 offset-md-6 block_2_2">
+                        <?php get_sidebar(); ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
         <div class="container">
             <div class="row">
                 <div class="col-md-9">
@@ -34,19 +82,6 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                                 ?>
 
                             <?php endif; ?>
-
-                            <div class="entry-content">
-                                <?php
-                                the_content();
-
-                                if ( ! $is_page_builder_used )
-                                    wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'Divi' ), 'after' => '</div>' ) );
-                                ?>
-                            </div> <!-- .entry-content -->
-
-                            <?php
-                            if ( ! $is_page_builder_used && comments_open() && 'on' === et_get_option( 'divi_show_pagescomments', 'false' ) ) comments_template( '', true );
-                            ?>
 
                     <?php endwhile; ?>
 
