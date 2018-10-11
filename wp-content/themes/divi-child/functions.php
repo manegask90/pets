22437da18644e8755461a3da814d5f23bf68f588
Vitalik M <vitalikmihaylishin@gmail.com>
2018-10-11 19:42:52 +0300

    single post

--- wp-content/themes/divi-child/functions.php
+++ wp-content/themes/divi-child/functions.php
@@ -307,3 +307,33 @@ function true_load_posts(){
 
 add_action('wp_ajax_loadmore', 'true_load_posts');
 add_action('wp_ajax_nopriv_loadmore', 'true_load_posts');
+
+
+
+
+//Limit words
+function excerpt($limit) {
+  $excerpt = explode(' ', get_the_excerpt(), $limit);
+  if (count($excerpt)>=$limit) {
+    array_pop($excerpt);
+    $excerpt = implode(" ",$excerpt);
+  } else {
+    $excerpt = implode(" ",$excerpt);
+  }
+  $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+  return $excerpt;
+}
+
+function content($limit) {
+  $content = explode(' ', get_the_content(), $limit);
+  if (count($content)>=$limit) {
+    array_pop($content);
+    $content = implode(" ",$content).'...';
+  } else {
+    $content = implode(" ",$content);
+  }
+  $content = preg_replace('/[.+]/','', $content);
+  $content = apply_filters('the_content', $content);
+  $content = str_replace(']]>', ']]&gt;', $content);
+  return $content;
+}
--- wp-content/themes/divi-child/single — copy.php
+++ wp-content/themes/divi-child/single — copy.php
@@ -7,7 +7,8 @@ $show_default_title = get_post_meta( get_the_ID(), '_et_pb_show_title', true );
 $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
 
 ?>
-    <div class="single-post">
+
+    <div id="main-content">
         <?php
         if ( et_builder_is_product_tour_enabled() ):
             // load fullwidth page in Product Tour mode
@@ -27,75 +28,20 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
         else:
             ?>
             <div class="container">
-                <div class="row">
+                <div id="content-area" class="clearfix">
+                    <div id="left-area">
                         <?php while ( have_posts() ) : the_post(); ?>
                             <?php setPostViews(get_the_ID()); ?>
                             <?php if (et_get_option('divi_integration_single_top') <> '' && et_get_option('divi_integrate_singletop_enable') == 'on') echo(et_get_option('divi_integration_single_top')); ?>
-
-                            <div class="col-md-12 header-block-wrap">
-                                <div class="post-thumb">
-                                    <?php the_post_thumbnail(); ?>
-                                </div>
-                                <div class="post-header-block">
-                                    <div class="post-header-block-wrap">
-                                        <div class="title-block">
-                                            <h2 class="entry-title"><?php the_title(); ?></h2>
-                                        </div>
-                                        <div class="text-block">
-                                            <?php echo excerpt(11); ?>
-                                            <div class="meta-box">
-                                                <div class="date">
-                                                    <?php the_date(); ?>
-                                                </div>
-                                                <div class="view">
-                                                    <i class="icon icon-eye"></i>
-                                                    <?php  echo getPostViews(get_the_ID()); ?>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-12">
-                                <div class="single-content">
-                                    <div class="block-bottom-header"></div>
-                                    <div class="social-box">
-                                        <?php get_template_part( '/includes/social_icons' ); ?>
-                                    </div>
-                                    <div class="single-entry-content">
-                                        <?php the_content(); ?>
-                                    </div>
-                                </div>
-                            </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
                             <article id="post-<?php the_ID(); ?>" <?php post_class( 'et_pb_post' ); ?>>
                                 <?php if ( ( 'off' !== $show_default_title && $is_page_builder_used ) || ! $is_page_builder_used ) { ?>
                                     <div class="et_post_meta_wrapper">
-<!--                                        <h1 class="entry-title">--><?php //the_title(); ?><!--</h1>-->
+                                        <h1 class="entry-title"><?php the_title(); ?></h1>
 
                                         <?php
                                         if ( ! post_password_required() ) :
 
-                                            // et_divi_post_meta();
+                                            et_divi_post_meta();
 
                                             $thumb = '';
 
@@ -206,8 +152,9 @@ $is_page_builder_used = et_pb_is_pagebuilder_used( get_the_ID() );
                             </article> <!-- .et_pb_post -->
 
                         <?php endwhile; ?>
+                    </div> <!-- #left-area -->
 
-                    <?php //get_sidebar(); ?>
+                    <?php get_sidebar(); ?>
                 </div> <!-- #content-area -->
             </div> <!-- .container -->
         <?php endif; ?>
