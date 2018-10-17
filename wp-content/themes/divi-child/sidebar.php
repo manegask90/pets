61fe6beccec10f6f5a0913662ae75ecd5a883e53
Vitalik M <vitalikmihaylishin@gmail.com>
2018-10-17 10:17:12 +0300

    add current events for news

--- wp-content/themes/divi-child/sidebar.php
+++ wp-content/themes/divi-child/sidebar.php
@@ -42,35 +42,36 @@
     <div class="sidebar_events_box">
         <h3 class="sidebar_header">Найближчі події</h3>
         <ul class="sidebar_events-list">
-            <li>
-                <a href="#" class="event_url">
-                    <span class="event_time">14.04.2017</span>
-                    <span class="event_tittle">Змагання службових вівчарок з усіх регіонів України</span>
-                    <i class="demo-icon icon-arrow-right"></i>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="event_url">
-                    <span class="event_time">14.04.2017</span>
-                    <span class="event_tittle">Виставка котів України та Европи</span>
-                    <i class="demo-icon icon-arrow-right"></i>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="event_url">
-                    <span class="event_time">14.04.2017</span>
-                    <span class="event_tittle">Змагання службових вівчарок з усіх регіонів України</span>
-                    <i class="demo-icon icon-arrow-right"></i>
-                </a>
-            </li>
-            <li>
-                <a href="#" class="event_url">
-                    <span class="event_time">14.04.2017</span>
-                    <span class="event_tittle">Змагання службових вівчарок з усіх регіонів України</span>
-                    <i class="demo-icon icon-arrow-right"></i>
-                </a>
-            </li>
-
+            <?php
+            $args = array(
+                'posts_per_page' => 4,
+                'category'       => 17,
+                'orderby'        => 'meta_value_num',
+                'order'          => 'ASC',
+                'meta_query'     => array(
+                    array(
+                        'key'     => 'date',
+//                        'compare'  => 'REGEXP',
+//                        'value'    => '[0-9]{4}' . $month . '[0-9]{2}',
+                    )
+                )
+            );
+            query_posts($args);
+            if (have_posts()) :
+            while (have_posts()) : the_post();
+            $date = get_field('date');
+            ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>" class="event_url">
+                        <span class="event_time"><?php echo $date; ?></span>
+                        <span class="event_tittle"><?php the_title(); ?></span>
+                        <i class="demo-icon icon-arrow-right"></i>
+                    </a>
+                </li>
+            <?php
+            endwhile;
+            endif;
+            ?>
         </ul>
     </div>
 </div>
